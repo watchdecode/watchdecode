a9f5caff9e05f574b09c4cfb67f35ae87bebbfb5
Kumar Satyam <kumarsatyam@Kumars-MacBook-Air.local>
2026-03-25 01:24:29 +0530

    fix: force dynamic route for keystatic api

--- app/api/keystatic/[...params]/route.ts
+++ app/api/keystatic/[...params]/route.ts
@@ -1,7 +1,8 @@
 import { makeRouteHandler } from "@keystatic/next/route-handler";
-
 import config from "../../../../keystatic.config";
 
 export const runtime = "nodejs";
+// ADD THIS LINE BELOW
+export const dynamic = "force-dynamic"; 
 
 export const { GET, POST } = makeRouteHandler({ config });
