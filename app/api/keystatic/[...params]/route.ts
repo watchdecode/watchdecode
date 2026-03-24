8ef9a51281896cbe5fa4b1efa4ee5dff4f64948f
Kumar Satyam <kumarsatyam@Kumars-MacBook-Air.local>
2026-03-25 00:57:06 +0530

    switch keystatic to github mode with auth

--- app/api/keystatic/[...params]/route.ts
+++ app/api/keystatic/[...params]/route.ts
@@ -2,4 +2,6 @@ import { makeRouteHandler } from "@keystatic/next/route-handler";
 
 import config from "../../../../keystatic.config";
 
+export const runtime = "nodejs";
+
 export const { GET, POST } = makeRouteHandler({ config });
