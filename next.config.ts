9f20186028e352ac6abff28055a5398edcc33e18
Kumar Satyam <watchdecode@gmail.com>
2026-03-26 17:39:19 +0530

    add canonical URL and www redirect

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,21 @@ const withMDX = createMDX({
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1"],
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.watchdecode.com",
+          },
+        ],
+        destination: "https://watchdecode.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withMDX(nextConfig);
