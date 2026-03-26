04d169ba15e0cad55d84f3233d3d6bcd80a910e9
Kumar Satyam <watchdecode@gmail.com>
2026-03-26 17:32:43 +0530

    add robots.txt blocking keystatic

--- app/robots.ts
+++ app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: "/keystatic/",
     },
     sitemap: "https://watchdecode.com/sitemap.xml",
   };
